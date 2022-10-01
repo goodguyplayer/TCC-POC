e1c31fbfa76c9d6d3294e2d65af00c12418621b8
caio-cruz-garcia <caiogarcia96@hotmail.com>
2022-10-01 09:42:02 -0300

    first implementation with IA

--- tccpoc/src/pages/Home/index.js
+++ tccpoc/src/pages/Home/index.js
@@ -7,6 +7,7 @@ const Home = () =>{
     const handleUpload = async () => {
         try {
           const upload = await axios.post('http://127.0.0.1:8000/logs', { FlowID: "192.168.4.118-203.73.24.75-4504-80-6" })
+          await axios.post('http://127.0.0.1:5000/IA', { FlowID: "192.168.4.118-203.73.24.75-4504-80-7" })
         } catch (e) {
           console.log(e)
         }

0fb879d83e2481435fc9cff37124485bfd529aef
caio-cruz-garcia <caiogarcia96@hotmail.com>
2022-10-17 11:46:57 -0300

    create pagination in grids

--- tccpoc/src/components/Table/index.jsx
+++ tccpoc/src/components/Table/index.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from '@mui/material';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, TableFooter, TablePagination, Paper } from '@mui/material';
 
-export const DataTable = ({ rows }) => {    
+export const DataTable = ({ rows }) => {
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+    // Avoid a layout jump when reaching the last page with empty rows.
+    const emptyRows =
+        page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+    const handleChangePage = (event, newPage) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
     return (
         <TableContainer component={Paper}>
             <Table sx={{ minWidth: 650 }} aria-label="caption table">
@@ -12,13 +28,40 @@ export const DataTable = ({ rows }) => {
                     </TableRow>
                 </TableHead>
                 <TableBody>
-                    {rows?.map(({ type, value }, key) => (
-                        <TableRow key={key}>
-                            <TableCell align="center">{type}</TableCell>
-                            <TableCell align="center">{value}</TableCell>
+                    {(rowsPerPage > 0
+                        ? rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                        : rows)?.map(({ type, value }, key) => (
+                            <TableRow key={key}>
+                                <TableCell align="center">{type}</TableCell>
+                                <TableCell align="center">{value}</TableCell>
+                            </TableRow>
+                        ))}
+
+                    {emptyRows > 0 && (
+                        <TableRow style={{ height: 53 * emptyRows }}>
+                            <TableCell colSpan={6} />
                         </TableRow>
-                    ))}
+                    )}
                 </TableBody>
+                <TableFooter>
+                    <TableRow>
+                        <TablePagination
+                            rowsPerPageOptions={[5, 10, 25, { label: 'All', value: -1 }]}
+                            colSpan={3}
+                            count={rows.length}
+                            rowsPerPage={rowsPerPage}
+                            page={page}
+                            SelectProps={{
+                                inputProps: {
+                                    'aria-label': 'rows per page',
+                                },
+                                native: true,
+                            }}
+                            onPageChange={handleChangePage}
+                            onRowsPerPageChange={handleChangeRowsPerPage}
+                        />
+                    </TableRow>
+                </TableFooter>
             </Table>
         </TableContainer>
     );
